75beb51ddc67ab7d9fbe7fbea84a2ae29218fa57
落花落叶 <99370657@qq.com>
2019-03-07 19:01:58 +0800

    Update initial-server.sh

--- initial-server.sh
+++ initial-server.sh
@@ -9,10 +9,6 @@ export PATH
 #	address: https://github.com/sxlszzy/Simple-Application-Server/
 #=================================================
 #定义变量
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
 
 
 #判断系统
@@ -57,4 +53,13 @@ function byellow(){
 
 green ("hello world with green color")
 blue "hello world with blue color"	
+
+# 颜色
+blue='\033[0;34m'
+yellow='\033[0;33m'
+green='\033[0;32m'
+red='\033[0;31m'
+plain='\033[0m'
+
 echo -e "${green}[完成] ${plain}宝塔面板破解版已安装成功！"
+echo "按脚本提供的后台入口、账号、密码，登录宝塔面板并使用！"
