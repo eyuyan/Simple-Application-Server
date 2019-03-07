2fdab24dd3554920eab6ae257341ebdd070dfe10
落花落叶 <99370657@qq.com>
2019-03-07 19:05:40 +0800

    Update initial-server.sh

--- initial-server.sh
+++ initial-server.sh
@@ -25,35 +25,7 @@ fi
 
 #安装依赖
 
-
-
-
-green (hello world with green color)
-blue "hello world with blue color"
-
-bred "error info with blink"
-byellow "warning info with blink"
-#定义颜色
-## green to echo 
-function green(){
-    echo -e "\033[32m[ $1 ]\033[0m"
-}
-## blue to echo 
-function blue(){
-    echo -e "\033[35m[ $1 ]\033[0m"
-}
-## Error to warning with blink
-function bred(){
-    echo -e "\033[31m\033[01m\033[05m[ $1 ]\033[0m"
-}
-## Error to warning with blink
-function byellow(){
-    echo -e "\033[33m\033[01m\033[05m[ $1 ]\033[0m"
-}
-
-green ("hello world with green color")
-blue "hello world with blue color"	
-
+	
 # 颜色
 blue='\033[0;34m'
 yellow='\033[0;33m'
