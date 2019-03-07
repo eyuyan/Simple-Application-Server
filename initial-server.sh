9b9d8a8b22a4294e1d7e4f2b5414a94ed0d76efc
落花落叶 <99370657@qq.com>
2019-03-07 19:14:18 +0800

    Update initial-server.sh

--- initial-server.sh
+++ initial-server.sh
@@ -16,13 +16,11 @@ if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
 	echo " 脚本已经测试系统centos7.3"
 	echo -e “\033[40;37m 黑底白字 \033[0m” 	
 else
-	echo "暂时只支持centos"
+
+	echo -e “\033[42;37m 暂时只支持centos \033[0m” 
 	exit 1  
 fi
-
-#安装依赖
-
-	
+#安装依赖	
 # 颜色
 blue='\033[0;34m'
 yellow='\033[0;33m'
